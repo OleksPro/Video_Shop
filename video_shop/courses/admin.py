783c477be6ef03778747ff70ebff338c7ef4f0c5
Oleksandr Prokopenko <Oleksandr.Prokopenko92@gmail.com>
2023-11-07 18:24:55 +0200

    Display lessons for the course

--- video_shop/courses/admin.py
+++ video_shop/courses/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Lesson
 
 # Регістрація таблиці Course в адмін-панелі
 admin.site.register(Course)
+admin.site.register(Lesson)
+
